ad3bc400fd2ffa4d34be5c570fda677dedd5db42
q474890522 <474890522@qq.com>
2018-08-19 04:28:49 +0800

    test pull push fetch merge

--- src/main/java/com/nowcoder/utils/ToutiaoUtil.java
+++ src/main/java/com/nowcoder/utils/ToutiaoUtil.java
@@ -11,14 +11,14 @@ import java.util.Map;
 public class ToutiaoUtil {
     private static final Logger logger = LoggerFactory.getLogger(ToutiaoUtil.class);
 
-    public static String IMAGE_DOMIN = "http://127.0.0.1:8080/";
+    public static String IMAGE_DOMIN = "http://127.0.0.1:8080/"; 
     public  static String IMAGE_DIR = "G:\\ToutiaoImage\\";
     public static String[] IMAGE_FILE_EXT = {"png", "jpg", "jpeg", "bmp"};
     public static boolean isImage(String fileExt) {
         for(String ext : IMAGE_FILE_EXT) {
             if(ext.equals(fileExt))
                 return true;
-        }
+        } 
         return false;
     }
 
